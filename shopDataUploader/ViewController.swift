c2a2997dc9ee35347a33928c9e3522c367ed0b41
Kyojin Syo <kyojinsyo@gmail.com>
2017-03-23 18:09:31 +0900

    Initial Commit

---
+++ shopDataUploader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shopDataUploader
+//
+//  Created by 蕭　喬仁 on 2017/03/23.
+//  Copyright © 2017年 蕭　喬仁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
